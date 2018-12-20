eeefbcbdeec956966f9231e838629df010c66953
Jean-Claude Wippler <jc@wippler.nl>
2018-12-20 10:41:13 +0100

    add zexall port to f407

---
+++ black407-z80ex/src/main.cpp
@@ -0,0 +1,73 @@
+#include <jee.h>
+#include <string.h>
+
+extern "C" {
+#include "context.h"
+#include "z80emu.h"
+}
+
+const uint8_t rom [] = {
+#include "zexall.h"
+};
+
+UartBufDev< PinA<9>, PinA<10> > console;
+
+int printf(const char* fmt, ...) {
+    va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
+    return 0;
+}
+
+PinA<6> led;
+Context context;
+
+void systemCall (Context *ctx, int) {
+    auto& regs = ctx->state.registers;
+
+    // emulate CP/M bdos calls from 0x0005, register C functions 2 and 9
+    switch (regs.byte[Z80_C]) {
+
+        case 2: // output the character in E
+            console.putc(regs.byte[Z80_E]);
+            break;
+
+        case 9: // output the string in DE until '$' terminator
+            for (int i = regs.word[Z80_DE]; *mapMem(ctx, i) != '$'; ++i)
+                console.putc(*mapMem(ctx, i));
+            break;
+    }
+}
+
+int main() {
+    console.init();
+    console.baud(115200, fullSpeedClock()/2);
+    led.mode(Pinmode::out);
+
+    memcpy(mapMem(&context, 0x100), rom, sizeof rom);
+
+    // Patch the memory of the program. Reset at 0x0000 is trapped by an
+    // OUT which will stop emulation. CP/M bdos call 5 is trapped by an IN.
+    // See Z80_INPUT_BYTE() and Z80_OUTPUT_BYTE() definitions in z80user.h.
+
+    *mapMem(&context, 0) = 0xd3;       // OUT N, A
+    *mapMem(&context, 1) = 0x00;
+
+    *mapMem(&context, 5) = 0xdb;       // IN A, N
+    *mapMem(&context, 6) = 0x00;
+    *mapMem(&context, 7) = 0xc9;       // RET
+
+    // start emulating
+
+    Z80Reset(&context.state);
+    context.state.pc = 0x100;
+    context.done = 0;
+
+    uint32_t start = ticks;
+    do {
+        Z80Emulate(&context.state, 2000000, &context);
+        led.toggle();
+    } while (!context.done);
+    printf("\nEmulating zexall took %d ms.\n", ticks - start);
+
+    led = 1;  // turn LED off (inverted logic)
+    while (true) {}
+}
