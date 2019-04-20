8864b48322beac446430e03c2af1ee23d2b2cd41
Jean-Claude Wippler <jc@wippler.nl>
2019-04-21 00:30:01 +0200

    make a number of blink demos work

--- tests/blink-esp/src/main.cpp
+++ tests/blink-esp/src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 
+#if LOLIN32
+constexpr int LED = 22; // not 5!
+#else
+constexpr int LED = BUILTIN_LED;
+#endif
+
 void setup() {
     Serial.begin(115200);
-    pinMode(22, OUTPUT);
+    delay(1000);
+    Serial.printf("BUILTIN_LED = %d\n", BUILTIN_LED);
+    delay(1000);
+    pinMode(LED, OUTPUT);
 }
 
 void loop() {
     Serial.printf("%lu\n", millis());
 
-    digitalWrite(22, 0);
+    digitalWrite(LED, 0);
     delay(100);
-    digitalWrite(22, 1);
+    digitalWrite(LED, 1);
     delay(400);
 }
