fde871460864a8f8856651fa432e16d19cc28fbe
Jean-Claude Wippler <jc@wippler.nl>
2019-04-22 22:54:02 +0200

    cleanup

--- tests/wrover-kit-lcd/src/main.cpp
+++ tests/wrover-kit-lcd/src/main.cpp
@@ -16,19 +16,11 @@
 
 #include "SPI.h"
 #include "Adafruit_GFX.h"
-//#include "Adafruit_ILI9341.h"
 #include "WROVER_KIT_LCD.h"
 
-// For the Adafruit shield, these are the default.
-#define TFT_DC 9
-#define TFT_CS 10
-
-// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-// If using the breakout, change pins as desired
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
-
 WROVER_KIT_LCD tft;
+
+#define yield()
 
 unsigned long testFillScreen() {
   unsigned long start = micros();
@@ -360,7 +352,9 @@ void setup() {
 void loop(void) {
   for(uint8_t rotation=0; rotation<4; rotation++) {
     tft.setRotation(rotation);
+    uint32_t t = micros();
     testText();
-    delay(1000);
+    Serial.println(micros() - t);
+    delay(3000);
   }
 }
