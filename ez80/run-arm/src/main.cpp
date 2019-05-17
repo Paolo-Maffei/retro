22039bed678dd99b90d6bfa3f64ec6421dc3ead8
Jean-Claude Wippler <jc@wippler.nl>
2019-05-17 12:18:58 +0200

    turn off RAM wait states

--- ez80/run-arm/src/main.cpp
+++ ez80/run-arm/src/main.cpp
@@ -9,6 +9,7 @@
 //  PA3 = eZ80 TX0, pin 73
 
 #include <jee.h>
+#include <string.h>
 
 UartBufDev< PinA<9>, PinA<10> > console;
 UartBufDev< PinA<2>, PinA<3> > serial;
@@ -25,6 +26,8 @@ PinB<2> ZDA;
 PinB<4> ZCL;
 PinB<8> RST;
 
+#include <zdi-util.h>
+
 void zdiConfig () {
     XIN.mode(Pinmode::alt_out); // XXX alt_out_50mhz
     // generate a 36 MHz signal with 50% duty cycle on PB0, using TIM3
@@ -32,14 +35,10 @@ void zdiConfig () {
     timer.pwm(1);
 
     // initialise all the main control pins
-    RST = 0; ZCL = 1; ZDA = 1;
+    RST = 1; ZCL = 1; ZDA = 0;
     RST.mode(Pinmode::out_od);
     ZCL.mode(Pinmode::out); // XXX out_50mhz
     ZDA.mode(Pinmode::out);
-
-    // keep ez80 in reset, briefly
-    wait_ms(2);
-    RST = 1;
 }
 
 int main() {
@@ -48,6 +47,7 @@ int main() {
     const int hz = fullSpeedClock();
     console.baud(115200, hz);
     serial.baud(9600 * 36/4, hz/2);
+    wait_ms(100);
 
     // disable JTAG in AFIO-MAPR to release PB3, PB4, and PA15
     // (looks like this has to be done *after* some GPIO mode inits)
@@ -55,6 +55,15 @@ int main() {
     MMIO32(afio+0x04) |= (2<<24); // disable JTAG, keep SWD enabled
 
     zdiConfig();
+    ezReset();
+
+    // no wait states for external ram (default is 7)
+    zIns(0x3E,0x08);        // ld a,08h
+    zIns(0xED, 0x39, 0xAA); // out0 (0AAh),a
+
+    // start running from flash
+    setPC(0x000000);        // PC was messed up by zIns
+    zdiOut(0x10, 0x00);     // continue
 
     while (true) {
         if (serial.readable())
