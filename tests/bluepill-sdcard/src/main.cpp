68327a37e205587a8d0c27358d7a1f412c9a26ae
Jean-Claude Wippler <jc@wippler.nl>
2019-05-28 22:33:02 +0200

    measure and optimise SD card I/O performance

--- tests/bluepill-sdcard/src/main.cpp
+++ tests/bluepill-sdcard/src/main.cpp
@@ -13,7 +13,11 @@ int printf(const char* fmt, ...) {
 
 PinC<13> led;
 
-SpiGpio< PinB<15>, PinB<14>, PinB<13>, PinB<12> > spi;
+#if SPI_1
+SpiHw< PinA<7>, PinA<6>, PinA<5>, PinA<4> > spi;
+#else
+SpiHw< PinB<15>, PinB<14>, PinB<13>, PinB<12> > spi;
+#endif
 SdCard< decltype(spi) > sd;
 
 int main() {
@@ -30,11 +34,15 @@ int main() {
     wait_ms(10);
     console.baud(115200, fullSpeedClock());
 
+    // switch to div-2, i.e. 36 MHz for SPI1, or 18 MHz for SPI2
+    MMIO32(spi.cr1) = (1<<6) | (0<<3) | (1<<2) | (0<<1);  // [1] p.742
+
     uint8_t buf [512];
 
     for (int block = 0; ; block += 256) {
         printf("\nblock = %d: ", block);
 
+        uint32_t t1 = ticks;
         memset(buf, 0, sizeof buf);
         for (int i = 0; i < 256; ++i) {
             buf[i] = i;
@@ -44,6 +52,7 @@ int main() {
             led.toggle();
         }
 
+        uint32_t t2 = ticks;
         memset(buf, 0, sizeof buf);
         for (int i = 0; i < 256; ++i) {
             sd.read512(block + i, buf);
@@ -51,5 +60,9 @@ int main() {
             console.putc(ok ? '+' : '?');
             led.toggle();
         }
+
+        uint32_t t3 = ticks;
+        printf("\nwr %d µs/blk, rd %d µs/blk",
+                ((t2-t1)*1000)/256, ((t3-t2)*1000)/256);
     }
 }
