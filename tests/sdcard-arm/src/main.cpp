4d16f9b193dfea0d39899c89f184be38c68f2020
Jean-Claude Wippler <jc@wippler.nl>
2019-05-29 02:43:45 +0200

    add circle103 sdcard test and some dir renames

--- tests/sdcard-arm/src/main.cpp
+++ tests/sdcard-arm/src/main.cpp
@@ -11,26 +11,50 @@ int printf(const char* fmt, ...) {
     return 0;
 }
 
-PinB<9> led;
+PinC<13> led;
 
+#if SPI_1
+SpiHw< PinA<7>, PinA<6>, PinA<5>, PinA<4> > spi;
+#elif SPI_2
+SpiHw< PinB<15>, PinB<14>, PinB<13>, PinB<12> > spi;
+#elif CIRCLE103 || CIRCLE407
+// all pins connected, i.e. could also use SDIO
+// see schematic: STM32F103/407VET6mini (circle)
 SpiGpio< PinD<2>, PinC<8>, PinC<12>, PinC<11> > spi;
+#endif
+
 SdCard< decltype(spi) > sd;
 
 int main() {
     console.init();
-    console.baud(115200, fullSpeedClock()/2);
+    enableSysTick();
     led.mode(Pinmode::out);
 
+    wait_ms(500);
     printf("\nsd card: ");
     spi.init();
     if (sd.init())
         printf("detected, hd=%d\n", sd.sdhc);
+
+    // switch to full speed, now that the SD card has been inited
+    wait_ms(10); // let serial output drain
+#if STM32F1
+    console.baud(115200, fullSpeedClock());
+#elif STM32F4
+    console.baud(115200, fullSpeedClock()/2);
+#endif
+
+#if SPI1 || SPI_2
+    // switch to div-2, i.e. 36 MHz for SPI1, or 18 MHz for SPI2 (on F103)
+    MMIO32(spi.cr1) = (1<<6) | (0<<3) | (1<<2) | (0<<1);  // [1] p.742
+#endif
 
     uint8_t buf [512];
 
     for (int block = 0; ; block += 256) {
         printf("\nblock = %d: ", block);
 
+        uint32_t t1 = ticks;
         memset(buf, 0, sizeof buf);
         for (int i = 0; i < 256; ++i) {
             buf[i] = i;
@@ -40,6 +64,7 @@ int main() {
             led.toggle();
         }
 
+        uint32_t t2 = ticks;
         memset(buf, 0, sizeof buf);
         for (int i = 0; i < 256; ++i) {
             sd.read512(block + i, buf);
@@ -47,5 +72,9 @@ int main() {
             console.putc(ok ? '+' : '?');
             led.toggle();
         }
+
+        uint32_t t3 = ticks;
+        printf("\nwr %d µs/blk, rd %d µs/blk",
+                ((t2-t1)*1000)/256, ((t3-t2)*1000)/256);
     }
 }
