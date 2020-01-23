d37d9ded606890268cac09ccf0719a6816d67f1f
Jean-Claude Wippler <jc@wippler.nl>
2020-01-23 01:32:41 +0100

    add exe header, remove unused code

--- asios/apps/src/main.cpp
+++ asios/apps/src/main.cpp
@@ -10,14 +10,36 @@ int printf(const char* fmt, ...) {
 //PinA<6> led;
 PinA<7> led;
 
+extern char _etext;
+extern char _edata;
+extern char _ebss;
 extern char _estack;
 extern "C" void Reset_Handler ();
 
-
+#if 1 // flash
 extern __attribute__ ((section(".isr_vec")))
 void (* const g_pfnVec[])() = {
     (void (*)()) &_estack, // stack pointer
     Reset_Handler,         // reset handler
+};
+#endif
+
+struct ExeHeader {
+    uint32_t magic;
+    uint32_t reset;
+    uint32_t etext, edata, ebss, estack;
+    uint32_t filler [2];    // header is 32 bytes
+};
+
+__attribute__ ((section(".exe_header")))
+ExeHeader g_exeHeader = {
+    0x12345678,
+    (uint32_t) Reset_Handler,
+    (uint32_t) &_etext,
+    (uint32_t) &_edata,
+    (uint32_t) &_ebss,
+    (uint32_t) &_estack,
+    0, 0,
 };
 
 int main() {
