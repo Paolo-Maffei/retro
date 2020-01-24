a8372aec77a812e6426d7c576ae1c80b63a5a13d
Jean-Claude Wippler <jc@wippler.nl>
2020-01-24 22:51:43 +0100

    add "context" peek test, not working yet

--- asios/peeks/src/context_main.cpp
+++ asios/peeks/src/context_main.cpp
@@ -1,9 +1,4 @@
 #include <jee.h>
-#include <string.h>
-
-const uint8_t appCode [] = {
-#include "appcode.h"
-};
 
 UartDev< PinA<9>, PinA<10> > console;
 
@@ -12,13 +7,10 @@ int printf(const char* fmt, ...) {
     return 0;
 }
 
-PinA<6> led;
-//PinA<7> led2;
-
 //------------------------------------------------------------------------------
 // see https://blog.stratifylabs.co/device/2013-10-09-Context-Switching-on-the-Cortex-M3/
 
-#define MAX_TASKS 10
+#define MAX_TASKS 5
 
 #define IN_USE_FLAG 0x1
 #define EXEC_FLAG   0x2
@@ -27,20 +19,20 @@ void context_switcher(void);
 void del_process(void);
 
 typedef struct {
-     void * sp; //The task's current stack pointer
+     void* sp; //The task's current stack pointer
      int flags; //Status flags includes activity status, parent task, etc
 } task_table_t;
-int current_task;
-task_table_t task_table[MAX_TASKS];
 
-static uint32_t * stack; //This is stored on the heap rather than the stack
+volatile task_table_t task_table[MAX_TASKS];
+volatile int current_task;
+volatile uint32_t* stack; //This is stored on the heap rather than the stack
 
 #define MAIN_RETURN 0xFFFFFFF9  //Tells the handler to return using the MSP
 #define THREAD_RETURN 0xFFFFFFFD //Tells the handler to return using the PSP
 
 //Reads the main stack pointer
-static inline void * rd_stack_ptr(void){
-  void * result=NULL;
+static inline void* rd_stack_ptr(void){
+  void* result=0;
   asm volatile ("MRS %0, msp\n\t"
       //"MOV r0, %0 \n\t"
       : "=r" (result) );
@@ -66,9 +58,10 @@ static inline void load_context(void){
 //The SysTick interrupt handler -- this grabs the main stack value then calls the context switcher
 void systick_handler(void){
     save_context();  //The context is immediately saved
-    stack = (uint32_t *)rd_stack_ptr();
+console.putc('.');
+    stack = (uint32_t*)rd_stack_ptr();
     ///if ( SysTick->CTRL & (1<16) ){ //Indicates timer counted to zero
-    if ( *(uint32_t*)0xE000E010 & (1<16) ){ //Indicates timer counted to zero
+    if (*(uint32_t*)0xE000E010 & (1<16) ){ //Indicates timer counted to zero
         context_switcher();
     }
     load_context(); //Since the PSP has been updated, this loads the last state of the new task
@@ -77,31 +70,34 @@ void systick_handler(void){
 //This does the same thing as the SysTick handler -- it is just triggered in a different way
 void pendsv_handler(void){
     save_context();  //The context is immediately saved
-    stack = (uint32_t *)rd_stack_ptr();
+console.putc('p');
+    stack = (uint32_t*)rd_stack_ptr();
     context_switcher();
     load_context(); //Since the PSP has been updated, this loads the last state of the new task
 }
 
 //This reads the PSP so that it can be stored in the task table
-static inline void * rd_thread_stack_ptr(void){
-    void * result=NULL;
+static inline void* rd_thread_stack_ptr(void){
+    void* result=0;
     asm volatile ("MRS %0, psp\n\t" : "=r" (result) );
     return(result);
 }
 
 //This writes the PSP so that the task table stack pointer can be used again
-static inline void wr_thread_stack_ptr(void * ptr){
+static inline void wr_thread_stack_ptr(void* ptr){
     asm volatile ("MSR psp, %0\n\t" : : "r" (ptr) );
 }
 
 //This is the context switcher
 void context_switcher(void){
+//console.putc('c');
    task_table[current_task].sp = rd_thread_stack_ptr(); //Save the current task's stack pointer
    do {
       current_task++;
       if ( current_task == MAX_TASKS ){
          current_task = 0;
-         *((uint32_t*)stack) = MAIN_RETURN; //Return to main process using main stack
+         //*((uint32_t*)stack) = MAIN_RETURN; //Return to main process using main stack
+         *((uint32_t*)stack) = THREAD_RETURN; //Use the thread stack upon handler return
          break;
       } else if ( task_table[current_task].flags & EXEC_FLAG ){ //Check exec flag
          //change to unprivileged mode
@@ -109,6 +105,7 @@ void context_switcher(void){
          break;
       }
    } while(1);
+//console.putc('0'+current_task);
    wr_thread_stack_ptr( task_table[current_task].sp ); //write the value of the PSP to the new task
 }
 
@@ -136,17 +133,18 @@ typedef struct {
   uint32_t r11;
 } sw_stack_frame_t;
 
-static char m_stack[sizeof(sw_stack_frame_t)];
+static char m_stack [2000] __attribute__((aligned(8)));
 
 void task_init(void){
     ///...
-    task_table[0].sp = m_stack + sizeof(sw_stack_frame_t);
+    task_table[0].sp = (uint8_t*) m_stack + sizeof m_stack;
+    printf("main sp 0x%08x\n", task_table[0].sp);
     ///....
     //The systick needs to be configured to the desired round-robin time
     //..when the systick interrupt fires, context switching will begin
 }
 
-int new_task(void *(*p)(void*), void * arg, void * stackaddr, int stack_size){
+int new_task(void* (*p)(void*), void* arg, void* stackaddr, int stack_size){
     int i;
     uint8_t* mem = (uint8_t*) stackaddr - stack_size; // start of app mem
     hw_stack_frame_t* process_frame;
@@ -155,7 +153,7 @@ int new_task(void *(*p)(void*), void * arg, void * stackaddr, int stack_size){
     __asm("cpsid if");
     for(i=1; i < MAX_TASKS; i++){
         if( task_table[i].flags == 0 ){
-            process_frame = (hw_stack_frame_t*) stackaddr - 1;
+            process_frame = ((hw_stack_frame_t*) stackaddr) - 1;
             process_frame->r0 = (uint32_t)arg;
             process_frame->r1 = 0;
             process_frame->r2 = 0;
@@ -165,10 +163,9 @@ int new_task(void *(*p)(void*), void * arg, void * stackaddr, int stack_size){
             process_frame->lr = (uint32_t)del_process;
             process_frame->psr = 0x21000000; //default PSR value
             task_table[i].flags = IN_USE_FLAG | EXEC_FLAG;
-            task_table[i].sp = mem +
-                stack_size -
-                sizeof(hw_stack_frame_t) -
-                sizeof(sw_stack_frame_t);
+            task_table[i].sp = ((sw_stack_frame_t*) process_frame) - 1;
+            printf("mem 0x%08x pf 0x%08x sp 0x%08x pc 0x%08x\n",
+                    mem, process_frame, task_table[i].sp, process_frame->pc);
             break;
         }
     }
@@ -185,10 +182,11 @@ int new_task(void *(*p)(void*), void * arg, void * stackaddr, int stack_size){
 
 //This is called when the task returns
 void del_process(void){
-  task_table[current_task].flags = 0; //clear the in use and exec flags
-  ///SCB->ICSR |= (1<<28); //switch the context
-  *(uint32_t*)0xE000ED04 |= (1<<28); //switch the context
-  while(1); //once the context changes, the program will no longer return to this thread
+console.putc('D');
+    task_table[current_task].flags = 0; //clear the in use and exec flags
+    ///SCB->ICSR |= (1<<28); //switch the context
+    *(uint32_t*)0xE000ED04 |= (1<<28); //switch the context
+    while(1); //once the context changes, the program will no longer return to this thread
 }
 
 //------------------------------------------------------------------------------
@@ -196,57 +194,29 @@ void del_process(void){
 int main () {
     console.init();
     //console.baud(115200, fullSpeedClock()/2);
-    enableSysTick(16000000/100);
+    enableSysTick();
     wait_ms(100);
-    //led.mode(Pinmode::out);
-
-    // e.g. https://github.com/dwelch67/stm32_samples/blob/master/NUCLEO-F767ZI/cpuid/README
-    for (int i = 0; i < 16; ++i)
-        printf("scb+0x%02x: 0x%08x = 0x%08x\n",
-            4*i, 0xE000ED00 + 4*i, ((const uint32_t*) 0xE000ED00)[i]);
-
-    VTableRam().sv_call = []() {
-        // console must be polled inside svc, i.e. UartDev iso UartBufDev
-        //printf("X %d\n", ticks);
-        console.putc('X');
-        // wait_ms() can't be used inside svc
-        //led = 0;
-        //for (int i = 0; i < 2000000; ++i) __asm("");
-        //led = 1;
-        //for (int i = 0; i < 20000000; ++i) __asm("");
-    };
-
-    uint32_t* appMem = (uint32_t*) 0x20000000;
-    memcpy(appMem, appCode, sizeof appCode);
-
-    printf("magic 0x%08x start 0x%08x staxk 0x%08x size %d b\n",
-            appMem[0], appMem[1], appMem[5], sizeof appCode);
+    enableSysTick(16000000/25);
 
     task_init();
 
-    VTableRam().systick = []() {
-        console.putc('.');
-        ++ticks;
-        systick_handler();
+    auto myTask = [](void*) -> void* {
+        console.putc('(');
+        for (int i = 0; i < 1000000; ++i) __asm("");
+        console.putc(')');
+        return 0;
     };
 
-    //static uint32_t am = appMem[1];
-    auto taskFun = [](void*) -> void* {
-        //console.putc('<');
-        //((int (*)()) am)();
-        //console.putc('>');
-        while (1)
-            console.putc('!');
-    };
-
-    static uint32_t tempStack [1000];
-    //int pid = new_task(taskFun, (void*) "abc", (void*) appMem[5], appMem[5] & 0xFFFFF);
-    int pid = new_task(taskFun, (void*) "abc", stack + sizeof stack, 4 * sizeof stack);
+    constexpr int N = 1000;
+    static uint8_t myStack [N] __attribute__((aligned(8)));
+    int pid = new_task(myTask, (void*) "abc", myStack + N, N);
     printf("pid %d\n", pid);
 
+    VTableRam().systick = systick_handler;
+    VTableRam().pend_sv = pendsv_handler;
+
     while (1) {
-        //printf("main %d\n", ticks);
         console.putc('M');
-        for (int i = 0; i < 2000000; ++i) __asm("");
+        for (int i = 0; i < 1000000; ++i) __asm("");
     }
 }
