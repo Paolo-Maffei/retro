3974918a811afce13bc66ee2af60e21a32ae05d8
Jean-Claude Wippler <jc@wippler.nl>
2020-02-06 12:25:20 +0100

    tweak

--- asios/kernel/lib/syslib/syslib.c
+++ asios/kernel/lib/syslib/syslib.c
@@ -1,10 +1,10 @@
 #define DEFINE_SYSCALLS
 #include "syslib.h"
 
-// small recplacement for the boot vector, since dispatch is now in the kernel
+// small replacement for the boot vector, since dispatch is now in the kernel
 extern char _estack[], Reset_Handler[];
 __attribute__ ((section(".boot_vector")))
 char* bootVector[] = { _estack, Reset_Handler };
 
-// disabled all privileged configuration code
+// need to disable all privileged configuration code
 void SystemInit (void) {}
