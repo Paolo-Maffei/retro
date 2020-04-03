bf30cec3d82672eb2805392e77f3b21705129fc0
Jean-Claude Wippler <jc@wippler.nl>
2020-04-03 22:10:36 +0200

    add Teensy 4 build, and measure performance

--- z80emu/zexall-arm/src/main.cpp
+++ z80emu/zexall-arm/src/main.cpp
@@ -1,5 +1,14 @@
+#if TEENSY
+#include <Arduino.h>
+#define ticks millis()
+#define printf Serial.printf
+#define PUTC Serial.write
+#else
+// FIXME comment this out for TEENSY, even though it's in the #else branch (!)
 #include <jee.h>
 #include <string.h>
+#define PUTC console.putc
+#endif
 
 extern "C" {
 #include "context.h"
@@ -10,6 +19,9 @@ const uint8_t rom [] = {
 #include "zexall.h"
 };
 
+#if TEENSY
+#define led 13
+#else
 UartBufDev< PinA<9>, PinA<10> > console;
 
 int printf(const char* fmt, ...) {
@@ -18,6 +30,7 @@ int printf(const char* fmt, ...) {
 }
 
 PinC<13> led;
+#endif
 Context context;
 
 void systemCall (Context *ctx, int, uint16_t) {
@@ -27,17 +40,21 @@ void systemCall (Context *ctx, int, uint16_t) {
     switch (regs.byte[Z80_C]) {
 
         case 2: // output the character in E
-            console.putc(regs.byte[Z80_E]);
+            PUTC(regs.byte[Z80_E]);
             break;
 
         case 9: // output the string in DE until '$' terminator
             for (int i = regs.word[Z80_DE]; *mapMem(ctx, i) != '$'; ++i)
-                console.putc(*mapMem(ctx, i));
+                PUTC(*mapMem(ctx, i));
             break;
     }
 }
 
-int main() {
+void setup () {
+#if TEENSY
+    Serial.begin(115200);
+    pinMode(led, OUTPUT);
+#else
     console.init();
 #if BLUEPILL
     int usartBusHz = fullSpeedClock();
@@ -46,6 +63,7 @@ int main() {
 #endif
     console.baud(115200, usartBusHz);
     led.mode(Pinmode::out);
+#endif
 
     memcpy(context.mem + 0x100, rom, sizeof rom);
 
@@ -66,13 +84,30 @@ int main() {
     context.state.pc = 0x100;
     context.done = 0;
 
+    uint64_t cycles = 0;
     uint32_t start = ticks;
-    do {
-        Z80Emulate(&context.state, 2000000, &context);
-        led.toggle();
-    } while (!context.done);
-    printf("\nEmulating zexall took %d ms.\n", ticks - start);
 
-    led = 1;  // turn LED off (inverted logic)
-    while (true) {}
+    do {
+        cycles += Z80Emulate(&context.state, 100000000, &context);
+#if TEENSY
+        digitalWrite(led, !digitalRead(led));
+#else
+        led.toggle();
+#endif
+    } while (!context.done);
+
+    uint32_t t = ticks - start;
+    printf("\nEmulating zexall took %.1f seconds: %llu cycles @ %.1f MHz\n",
+            t/1000.0, cycles, cycles/(1000.0*t));
+
+    // Teensy 4.0 @ 600 MHz:
+    //  Emulating zexall took 294.6 seconds: 46734978649 cycles @ 158.6 MHz
+}
+
+void loop () {}
+
+int main () {
+    setup();
+    while (true)
+        loop();
 }
